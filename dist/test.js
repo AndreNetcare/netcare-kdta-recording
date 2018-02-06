b6616775034957e0e580dd100126df723706760b
Andre Gluderer <andre.gluderer@netcare.de>
2018-02-06 13:33:34 +0100

    added element id, href, class to click listener

--- dist/test.js
+++ dist/test.js
@@ -10,12 +10,17 @@ window.addEventListener("click", function (e) {
     var y = e.y;
     console.log('x=' + x + ' y=' + y);
     var target = e.target || e.srcElement;
+    var id = e.target.getAttribute('id');
+    var elementclass = e.target.getAttribute('class');
+    var elementhref = e.target.getAttribute('href');
     updateCodeMirror2('clicked at: ' + 'x=' + x + ' y=' + y);
     //const {ipcRenderer} = require('electron');
     //var remote = require('remote');
     //var ipc = remote.require('ipc');
-    ipcRenderer.sendToHost(target + ' clicked at: ' + 'x=' + x + ' y=' + y);
+    ipcRenderer.sendToHost(target + ' clicked at: ' + 'x=' + x + ' y=' + y + ' - id: ' + id + ' class: ' + elementclass + ' href: ' + elementhref);
 });
+function targetInfo(target) {
+}
 window.addEventListener("keypress", function (e) {
     ipcRenderer.sendToHost('enterd: ' + e.key);
 });
--- src/test.ts
+++ src/test.ts
@@ -12,13 +12,19 @@ window.addEventListener ("click", function (e){
     var y = e.y;
     console.log('x=' + x + ' y=' + y);
     var target = e.target || e.srcElement;
-    
+    var id = (e.target as HTMLElement).getAttribute('id');
+    var elementclass = (e.target as HTMLElement).getAttribute('class');
+    var elementhref = (e.target as HTMLElement).getAttribute('href');
     updateCodeMirror2('clicked at: ' + 'x=' + x + ' y=' + y);
     //const {ipcRenderer} = require('electron');
     //var remote = require('remote');
     //var ipc = remote.require('ipc');
-    ipcRenderer.sendToHost(target + ' clicked at: ' + 'x=' + x + ' y=' + y);   
+    ipcRenderer.sendToHost(target + ' clicked at: ' + 'x=' + x + ' y=' + y + ' - id: ' + id + ' class: ' + elementclass + ' href: ' + elementhref);   
 });
+
+function targetInfo(target: EventTarget){
+    
+}
 
 window.addEventListener("keypress", function (e){
     
